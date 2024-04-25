3dde15e28e1aa33484aa4cecfc9178a83f28461c
Dmytro Marilovtsev <wowforlife4@gmail.com>
2024-04-25 15:51:48 +0300

    Some fix

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine
+Console.Write("C# top!");
+Console.WriteLine("Hi, Sun!");
+
+
 Console.Read();
