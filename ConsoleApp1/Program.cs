dfd3bc3ca566a2c3c25830249628fd441acc30b1
Dmytro Marilovtsev <wowforlife4@gmail.com>
2024-06-02 19:57:57 +0300

    New fix

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 
-string name = "Dmytro";
-string secondName = "Marilovtsev";
-Console.WriteLine("My name is " + name + "!"); //concatanation
-Console.WriteLine($"My second name is {secondName}"); //interpolation
-
-Console.Read();
+string name;
+string proffesion = "Automation";
