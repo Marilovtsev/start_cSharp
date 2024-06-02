edb40816dccc0db1d744306f12c0e953515e5dce
Dmytro Marilovtsev <wowforlife4@gmail.com>
2024-06-02 20:36:44 +0300

    data types

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Globalization;
 
-string name;
-string proffesion = "Automations";
+int age;
+age = 25;
+
+Console.WriteLine($"My age is {age}");
+
+double height = 1.72;
+double weight = 50;
+Console.WriteLine($"My height is {height}, and my weight is {weight}");
+
+/*Console.WriteLine("Please enter your name:");
+string name = Console.ReadLine();
+Console.WriteLine($"My name is {name}");*/
+
+char key = 'a';
+key = 'B';
+Console.WriteLine(key);
+
+//string emptyString = string.Empty;
+string emptyString = "";
+
+Console.WriteLine($"THIS IS EMPTY STRING ->{emptyString}!");
+
+string str1 = "test";
+string str2 = "123123";
+string str3 = "!@#$%";
+string str4 = "5";
+
+Console.WriteLine(str1);
+Console.WriteLine(str2);
+Console.WriteLine(str3);
+Console.WriteLine(str4);
+
+string stringWithQuotes = "This is string with \"double quotes\"";
+Console.WriteLine(stringWithQuotes);
